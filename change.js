e9147777a1de32923e52831f7f1fa7040ec45e26
Ramonee <107966914+PedroRamonee@users.noreply.github.com>
2023-07-26 17:52:42 -0300

    Add files via upload

---
+++ change.js
@@ -0,0 +1,377 @@
+function changeText(name) {
+
+    var a = document.querySelectorAll('.overlay p');
+    var nome = name.querySelector('span')
+
+    if (nome.innerText == 'Croc') {
+        a[0].innerHTML = 'Rensga 2021-2'
+        a[1].innerHTML = 'Miners 2022-1'
+        a[2].innerHTML = 'Loud 2022-2 --> Presente'
+    }
+
+    else if (nome.innerText == 'Route') {
+        a[0].innerHTML = 'Loud 2023-1 --> Presente'
+
+        for (var i = 1; i < a.length; i++) {
+            a[i].innerHTML = ''
+        }
+    }
+
+    else if (nome.innerText == 'Wizer') {
+
+        a[0].innerHTML = 'Kabum 2019-2'
+        a[1].innerHTML = 'Kabum 2021-1 --> 2021-2'
+        a[2].innerHTML = 'Pain 2022-1 --> Presente'
+    }
+    else if (nome.innerText == 'Bvoy') {
+
+        a[0].innerHTML = 'Flamengo 2020 - 2'
+        a[1].innerHTML = 'Pain 2023-1 --> Presente'
+        a[2].innerHTML = ''
+    }
+    else if (nome.innerText == 'Zzk') {
+
+        a[0].innerHTML = 'INTZ 2023-1 --> Presente'
+
+        for (var i = 1; i < a.length; i++) {
+            a[i].innerHTML = ''
+        }
+    }
+    else if (nome.innerText == 'Nia') {
+
+        a[0].innerHTML = 'INTZ 2023-1 --> Presente'
+
+        for (var i = 1; i < a.length; i++) {
+            a[i].innerHTML = ''
+        }
+    }
+    else if (nome.innerText == 'Lava') {
+
+        a[0].innerHTML = 'Los Grandes 2023-1 --> Presente'
+
+        for (var i = 1; i < a.length; i++) {
+            a[i].innerHTML = ''
+        }
+    }
+    else if (nome.innerText == 'Lonely') {
+
+        a[0].innerHTML = 'Kabum 2023-1 --> Presente'
+
+        for (var i = 1; i < a.length; i++) {
+            a[i].innerHTML = ''
+        }
+    }
+    else if (nome.innerText == 'Trick') {
+
+        a[0].innerHTML = 'Los Grandes 2023-2'
+
+        for (var i = 1; i < a.length; i++) {
+            a[i].innerHTML = ''
+        }
+    }
+    else if (nome.innerText == 'Flawless') {
+
+        a[0].innerHTML = 'Kabum 2023-2'
+
+        for (var i = 1; i < a.length; i++) {
+            a[i].innerHTML = ''
+        }
+    }
+    else if (nome.innerText == 'Yuri') {
+
+        a[0].innerHTML = 'Rensga 2021-2 '
+        a[1].innerHTML = 'Kabum 2023-1'
+        a[2].innerHTML = ''
+
+    }
+    else if (nome.innerText == 'Hirit') {
+
+        a[0].innerHTML = 'Los Grandes 2023-1 '
+
+        for (var i = 1; i < a.length; i++) {
+            a[i].innerHTML = ''
+        }
+    }
+    else if (nome.innerText == 'Kuri') {
+
+        a[0].innerHTML = 'Flamengo 2022-1 --> 2022-2 '
+        a[1].innerHTML = 'RED Canids 2023-1'
+        a[2].innerHTML = ''
+
+    }
+    else if (nome.innerText == 'Trap') {
+
+        a[0].innerHTML = 'Rensga 2022-1 --> 2022-2'
+
+        for (var i = 1; i < a.length; i++) {
+            a[i].innerHTML = ''
+        }
+    }
+    else if (nome.innerText == 'Mocha') {
+
+        a[0].innerHTML = 'Rensga 2022-1 --> 2022-2'
+
+        for (var i = 1; i < a.length; i++) {
+            a[i].innerHTML = ''
+        }
+    }
+    else if (nome.innerText == 'Geum Go') {
+
+        a[0].innerHTML = 'Flamengo 2022-2'
+
+        for (var i = 1; i < a.length; i++) {
+            a[i].innerHTML = ''
+        }
+    }
+    else if (nome.innerText == 'DoRun') {
+
+        a[0].innerHTML = 'Miners 2022-1 --> 2022-2'
+
+        for (var i = 1; i < a.length; i++) {
+            a[i].innerHTML = ''
+        }
+    }
+    else if (nome.innerText == 'Sive') {
+
+        a[0].innerHTML = 'Miners 2022-2'
+
+        for (var i = 1; i < a.length; i++) {
+            a[i].innerHTML = ''
+        }
+    }
+    else if (nome.innerText == 'Parang') {
+
+        a[0].innerHTML = 'Kabum 2020-1 '
+        a[1].innerHTML = 'Flamengo 2021-1 --> 2021-2'
+        a[2].innerHTML = 'Kabum 2022-1 --> 2022-2'
+
+    }
+    else if (nome.innerText == 'Wiz') {
+
+        a[0].innerHTML = 'Kabum 2020-1 '
+        a[1].innerHTML = 'Pain ???'
+        a[2].innerHTML = 'Kabum 2022-1 --> 2022-2'
+
+    }
+    else if (nome.innerText == 'Hades') {
+
+        a[0].innerHTML = 'Rensga 2022-1'
+
+        for (var i = 1; i < a.length; i++) {
+            a[i].innerHTML = ''
+        }
+    }
+    else if (nome.innerText == 'Luci') {
+
+        a[0].innerHTML = 'Flamengo 2019-1 --> 2020-2'
+        a[1].innerHTML = 'Pain 2021-1 --> 2021-2'
+        a[2].innerHTML = ''
+
+    }
+    else if (nome.innerText == 'Ryan') {
+
+        a[0].innerHTML = 'Kabum 2021-1 --> 2021-2'
+
+        for (var i = 1; i < a.length; i++) {
+            a[i].innerHTML = ''
+        }
+    }
+    else if (nome.innerText == 'Jackpot') {
+
+        a[0].innerHTML = 'Santos 2020-2'
+
+        for (var i = 1; i < a.length; i++) {
+            a[i].innerHTML = ''
+        }
+    }
+    else if (nome.innerText == 'Rainbow') {
+
+        a[0].innerHTML = 'Santos 2020-2'
+
+        for (var i = 1; i < a.length; i++) {
+            a[i].innerHTML = ''
+        }
+    }
+    else if (nome.innerText == 'Balkhan') {
+
+        a[0].innerHTML = 'Redemption 2020-1'
+
+        for (var i = 1; i < a.length; i++) {
+            a[i].innerHTML = ''
+        }
+    }
+    else if (nome.innerText == 'Patrick') {
+
+        a[0].innerHTML = 'Redemption 2020-1'
+
+        for (var i = 1; i < a.length; i++) {
+            a[i].innerHTML = ''
+        }
+    }
+    else if (nome.innerText == 'Woofe') {
+
+        a[0].innerHTML = 'Flamengo 2020-1'
+
+        for (var i = 1; i < a.length; i++) {
+            a[i].innerHTML = ''
+        }
+    }
+    else if (nome.innerText == 'Shrimp') {
+
+        a[0].innerHTML = 'Flamengo 2018-2 --> 2019-2'
+
+        for (var i = 1; i < a.length; i++) {
+            a[i].innerHTML = ''
+        }
+    }
+    else if (nome.innerText == 'SeongHwan') {
+
+        a[0].innerHTML = 'Pain 2020-1'
+
+        for (var i = 1; i < a.length; i++) {
+            a[i].innerHTML = ''
+        }
+    }
+    else if (nome.innerText == 'Key') {
+
+        a[0].innerHTML = 'Pain 2020-1'
+
+        for (var i = 1; i < a.length; i++) {
+            a[i].innerHTML = ''
+        }
+    }
+    else if (nome.innerText == 'Mental') {
+
+        a[0].innerHTML = 'CNB 2019-2'
+
+        for (var i = 1; i < a.length; i++) {
+            a[i].innerHTML = ''
+        }
+    }
+    else if (nome.innerText == 'Chaser') {
+
+        a[0].innerHTML = 'RED Canids 2018-2'
+
+        for (var i = 1; i < a.length; i++) {
+            a[i].innerHTML = ''
+        }
+    }
+    else if (nome.innerText == 'Sky') {
+
+        a[0].innerHTML = 'RED Canids 2018-1 --> 2018-2'
+        a[1].innerHTML = 'NOTA: Jogou o circuitão de 2020-1 na RED também'
+        a[2].innerHTML = ''
+
+    }
+    else if (nome.innerText == 'Jisu') {
+
+        a[0].innerHTML = 'Flamengo 2018-2'
+
+        for (var i = 1; i < a.length; i++) {
+            a[i].innerHTML = ''
+        }
+    }
+    else if (nome.innerText == 'Lactea') {
+
+        a[0].innerHTML = 'Pain 2014 (não existia divisão em splits)'
+        a[1].innerHTML = 'Team One 2018-1'
+        a[2].innerHTML = ''
+
+    }
+    else if (nome.innerText == 'DayDream') {
+
+        a[0].innerHTML = 'Keyd Stars 2015-1'
+
+        for (var i = 1; i < a.length; i++) {
+            a[i].innerHTML = ''
+        }
+    }
+    else if (nome.innerText == 'Emperor') {
+
+        a[0].innerHTML = 'Keyd Stars 2015-1'
+
+        for (var i = 1; i < a.length; i++) {
+            a[i].innerHTML = ''
+        }
+    }
+    else if (nome.innerText == 'Winged') {
+
+        a[0].innerHTML = 'Keyd Stars 2014 (não existia divisão em splits)'
+        a[1].innerHTML = 'RED Canids 2018-1'
+        a[2].innerHTML = ''
+
+    }
+    else if (nome.innerText == 'Suno') {
+
+        a[0].innerHTML = 'Keyd Stars 2014 (não existia divisão em splits)'
+
+        for (var i = 1; i < a.length; i++) {
+            a[i].innerHTML = ''
+        }
+    }
+    else if (nome.innerText == 'Olleh') {
+
+        a[0].innerHTML = 'Pain 2014 (não existia divisão em splits)'
+
+        for (var i = 1; i < a.length; i++) {
+            a[i].innerHTML = ''
+        }
+    }
+    else if (nome.innerText == 'Stardust') {
+
+        a[0].innerHTML = 'Flamengo 2020-1'
+        a[1].innerHTML = 'Los Grandes 2023-1 --> Presente'
+        a[2].innerHTML = ''
+    }
+    else if (nome.innerText == 'Reven/Flanalista') {
+
+        a[0].innerHTML = 'Flamengo 2019-1 --> 2020-2'
+        a[1].innerHTML = 'NOTA: A maior parte do tempo na ORG ele foi analista, contudo virou Strategic Coach em 2020-2'
+        a[2].innerHTML = ''
+    }
+    else if (nome.innerText == 'Xero') {
+
+        a[0].innerHTML = 'Pain 2020-1 --> Presente'
+        a[1].innerHTML = 'NOTA: Durante o primeiro split de 2021 Xero foi apenas consultor, ironicamente a ORG foi campeã nesse split, então tecnicamente, Xero nunca ganhou o cblol'
+        a[2].innerHTML = ''
+
+    }
+    else if (nome.innerText == 'Sickness') {
+
+        a[0].innerHTML = 'Kabum 2023-2'
+
+        for (var i = 1; i < a.length; i++) {
+            a[i].innerHTML = ''
+        }
+    }
+    else if (nome.innerText == 'Hiro') {
+
+        a[0].innerHTML = 'Kabum 2018-2 --> 2019-2'
+
+        for (var i = 1; i < a.length; i++) {
+            a[i].innerHTML = ''
+        }
+    }
+    else if (nome.innerText == 'Icarus') {
+
+        a[0].innerHTML = 'RED Canids 2018-1'
+
+        for (var i = 1; i < a.length; i++) {
+            a[i].innerHTML = ''
+        }
+    }
+    else if (nome.innerText == 'sBs') {
+
+        a[0].innerHTML = 'Furia 2023-2'
+
+        for (var i = 1; i < a.length; i++) {
+            a[i].innerHTML = ''
+        }
+    }
+
+
+
+
+    callOverlay(name)
+
+}
